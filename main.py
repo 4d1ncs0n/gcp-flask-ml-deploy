7aface83d15b625845e296dbd5bcdd71c217b690
Danny Velásquez <75057819+4d1ncs0n@users.noreply.github.com>
2022-02-11 10:39:26 -0500

    feat: testing continuous delivery

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def hello():
   """Return a friendly HTTP greeting.
   """
   print("I am inside hello world!")
-  return 'Hello World!'
+  return 'Hello World! CD'
 
 @app.route('/echo/<name>')
 def echo(name):
